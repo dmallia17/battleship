d2d3d7d44540487953f61b0461a26412d6468511
dmallia17 <33104112+dmallia17@users.noreply.github.com>
2019-03-01 22:45:14 -0500

    Initial upload - work in progress.

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+const compZones = document.querySelectorAll('.compBoard .zone');
+const playerZones = document.querySelectorAll('.playerBoard .zone');
+
+gameBegin();
+
+
+function gameBegin() {
+  document.querySelector(".gameOverDisplay").style.display = "none"; //Clears gameOverDisplay
+
+  //Clear compboard colors
+  for(let i = 0; i < compZones.length; i++) {
+    compZones[i].style.backgroundColor = "gray";
+  }
+
+  //Clear playerBoardcolors
+  for(let i = 0; i < playerZones.length; i++) {
+    playerZones[i].style.backgroundColor = "white";
+    playerZones[i].addEventListener('click', setup, false);
+  }
+}
+
+
+
+function setup(zone) {
+
+  placeCompShip();
+}
