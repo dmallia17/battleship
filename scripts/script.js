862653902b4e6c9dff4b0a1ea74cd593f8626da8
dmallia17 <33104112+dmallia17@users.noreply.github.com>
2019-03-07 14:56:38 -0500

    Version 1 Complete

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,10 @@ Project Name: Battleship
 File Name: script.js
 Author: Daniel Mallia and Jayson Tan
 Date Begun: 02/28/2019
-Last Edited: 03/05/2019
+Last Edited: 03/07/2019
 */
 
+//Global vars for board control and game state
 const compZones = document.querySelectorAll('.compBoard .zone');
 const playerZones = document.querySelectorAll('.playerBoard .zone');
 let playerShip = [];
@@ -14,30 +15,33 @@ let compChoices =[];
 
 gameBegin();
 
+//Initialize game state
 function gameBegin() {
   document.querySelector(".gameOverDisplay").style.display = "none"; //Clears gameOverDisplay
 
   //Clear game arrays
   playerShip = [];
   compShip =[];
+  compChoices =[];
 
   //Clear compboard colors
   for(let i = 0; i < compZones.length; i++) {
     compZones[i].style.backgroundColor = "gray";
   }
 
-  //Clear playerBoardcolors
+  //Clear playerBoardcolors and begin setup
   for(let i = 0; i < playerZones.length; i++) {
     playerZones[i].style.backgroundColor = "white";
     playerZones[i].addEventListener('click', setup, false);
   }
 }
 
+//Allow for placement of ships
 function setup(zone) {
   placePlayerShip(zone);
   placeCompShip()
 
-  if(playerShip.length === 2 && compShip.length === 2) {    // ensures playerShip is placed first                        
+  if(playerShip.length === 2 && compShip.length === 2) {    // Ensures playerShip is placed first
     for(let i = 0; i < playerZones.length; i++) {
       compZones[i].addEventListener('click', fire, false);
     }
@@ -45,15 +49,17 @@ function setup(zone) {
 }
 
 
-function placePlayerShip(zone) { 
+function placePlayerShip(zone) {
   if(playerShip.length === 0) {
     playerShip.push(Number(zone.target.id));
     zone.target.removeEventListener('click', setup, false);
     zone.target.style.backgroundColor = "green";
   }
   else if(playerShip.length === 1) {
+    // Check valid second half of ship placement
     if((Number(zone.target.id) === (playerShip[0] + 1)) || (Number(zone.target.id) === (playerShip[0] - 1)) ||
        (Number(zone.target.id) === (playerShip[0] + 5)) || (Number(zone.target.id) === (playerShip[0] - 5))) {
+      // Check no "wrap around" on board
       if(!(playerShip[0] % 5 === 0 && Number(zone.target.id) === playerShip[0] - 1) &&
          !(playerShip[0] % 5 === 4 && Number(zone.target.id) === playerShip[0] + 1)){
         playerShip.push(Number(zone.target.id));
@@ -66,14 +72,17 @@ function placePlayerShip(zone) {
   }
 }
 
+
 function placeCompShip() {
   if(compShip.length === 0) {
-    compShip.push(Math.floor(Math.random() * 24));
-    compZones[compShip[0]].style.backgroundColor = "green";
+    compShip.push(Math.floor(Math.random() * 24)); // Randomized ship placement
+    //compZones[compShip[0]].style.backgroundColor = "green"; //Reveal comp ship
+                                                              //for debugging
   }
   else if(compShip.length === 1) {
     while(compShip.length !== 2) {
-      let choice = Math.floor(Math.random() * 24);
+      let choice = Math.floor(Math.random() * 24); // Continue randomized ship
+                                                   // placement - valid & no repeat or wrap
       if((choice !== compShip[0]) &&
         (Number(choice) === compShip[0] + 1 || Number(choice) === compShip[0] - 1 ||
          Number(choice) === compShip[0] + 5 || Number(choice) === compShip[0] - 5)) {
@@ -81,41 +90,45 @@ function placeCompShip() {
         if(!(compShip[0] % 5 === 0 && Number(choice) === compShip[0] - 1) &&
            !(compShip[0] % 5 === 4 && Number(choice) === compShip[0] + 1)){
           compShip.push(choice);
-          compZones[compShip[1]].style.backgroundColor = "green";
+          //compZones[compShip[1]].style.backgroundColor = "green"; //Reveal comp ship
+                                                                  //for debugging
         }
       }
     }
   }
 }
 
+// "Fire" on enemy ship - player initiates, computer responds
 function fire(zone) {
   playerTurn(zone);
   compTurn();
 }
 
 function playerTurn(zone) {
-  let index = compShip.indexOf(Number(zone.target.id));
+  let index = compShip.indexOf(Number(zone.target.id)); //Check if "hit" on computer's
+                                                        //ship in zone.
   if(index > -1) {
     compZones[compShip[index]].style.backgroundColor = "red";
     compShip.splice(index, 1);
-    zone.target.removeEventListener('click', fire, false);
+    zone.target.removeEventListener('click', fire, false); //remove fire possibility
     checkWin();
   }
   else {
-    zone.target.removeEventListener('click', fire, false);    // prevents from clicking the same cell and then invoking pcTurn
+    zone.target.removeEventListener('click', fire, false); // prevents from clicking
+                                                           // same cell and invoking compTurn
     zone.target.style.backgroundColor = "blue";
   }
 }
 
 function compTurn() {
-  let choice = (Math.floor(Math.random() * 24)) + 25;
-  while (compChoices.indexOf(choice) > -1) {
-    console.log(compChoices.indexOf(choice))
+  let choice = (Math.floor(Math.random() * 24)) + 25; //Randomized firing
+  while (compChoices.indexOf(choice) > -1) { // Avoid prior choices
+    //console.log(compChoices.indexOf(choice)) // Log for debugging
     choice = (Math.floor(Math.random() * 24)) + 25;
   }
   compChoices.push(choice);
-  //console.log(choice);
-  let index = playerShip.indexOf(choice);
+  //console.log(choice); //Log for debugging
+  let index = playerShip.indexOf(choice); // Check "hit"
   if(index > -1) {
     playerZones[choice % 25].style.backgroundColor = "red";
     playerShip.splice(index, 1);
@@ -126,12 +139,10 @@ function compTurn() {
   }
 }
 
-function smartChoice () {
-  
-}
+//function smartChoice () {} //Later implementation
 
 function checkWin() {
-  if(playerShip.length === 0) {
+  if(playerShip.length === 0) { //If ship array empty, ship has been sunk
     gameOver("Computer Wins");
   }
   else if(compShip.length === 0) {
@@ -146,4 +157,3 @@ function gameOver(winner) {
   document.querySelector(".gameOverDisplay").style.display = "block";
   document.querySelector(".gameOverDisplay .Winner").innerText = winner;
 }
-
